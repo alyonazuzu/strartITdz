f4464d5fedd4b19fc798be7ffdbecb65e5e52b15
vtatarynov <vtatarynov@mobileiron.com>
2019-01-13 12:29:49 +0200

    first commit

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,13 @@
+public class FirstClass {
+
+    private String name = "Vitalii";
+    public static String staticVar = "I'm static variable";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
